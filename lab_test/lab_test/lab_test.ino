31d8643e791b55ec78be0a269dc87c784cc3267a
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2025-10-16 13:58:34 +0700

    release version 1.0.1 update lab_test file for test in 17/10/2568

--- lab_test/lab_test/lab_test.ino
+++ lab_test/lab_test/lab_test.ino
@@ -10,6 +10,9 @@
 #define ONE_WIRE_BUS 18
 #define CURRENT_PIN 35
 #define VOLTAGE_PIN 33
+#define LOAD_CELL_CLOCKPIN 22
+#define LOAD_CELL_DATA 23
+
 const float a_current = -0.0494;
 const float b_current = 1.0184;
 const float c_current = 0.0622;
@@ -23,8 +26,6 @@ float voltage_from_adc = ((raw_current / 4095) * 3.3);
 float voltage_without_offset = voltage_from_adc - OFFSET_CURRENT;
 float real_current = voltage_without_offset / 0.185;
 float sum_value = 0;
-uint8_t clockPin = 22;
-uint8_t dataPin = 23;
 
 // coefficient of linear calibraton สัมประสิทธิ์ของการปรับเทียบแบบเส้นตรง
 float a_voltage = 0.6676;
@@ -47,14 +48,11 @@ void ShowTemperatureFromSensor() {
   float raw_temp_c = sensors.getTempCByIndex(0);
   float real_temp_c = raw_temp_c - OFFSET_TEMP;
 
-  Serial.print("Raw Temperature : ");
-  Serial.print(raw_temp_c);
-  Serial.print("\t Real Temperature : ");
+  Serial.print("Temperature : ");
   Serial.println(real_temp_c);
 }
 
-// This function was created for show Current in unit [A: Ampere]
-void ShowCurrent() {
+void ShowDCPowerMeter() {
   float raw_current;
   float raw_without_offset;
   float voltage_from_adc;
@@ -66,43 +64,66 @@ void ShowCurrent() {
   for (int i = 0; i <= 1000; i++) {
     sum_value += real_current;  // <-- Loop for Summation Value. ลูปเพื่อเก็บค่าทั้งหมดตามจำนวนตัวอย่าง
   }
-
   float avg_current = sum_value / 1000;  // <-- Summation / Amount Sample. หาค่าเฉลี่ย = ค่าทั้งหมด / จำนวนตัวอย่าง.
+
   // <-- Polynomial Calibration การปรับเทียบแบบพหุนาม
   float calibrated_current = (a_current * avg_current * avg_current) + (b_current * avg_current) + c_current;
   Serial.print("Calibrated Current (I) : ");
   Serial.print(calibrated_current);
   Serial.println(" A");
-}
+  delay(500);
 
-void showVoltage() {
+  // Read ADC from Voltage Sensor อ่านค่าสัญญาณแอนาล็อกจาก Voltage Sensor
   int adc_voltage = analogRead(VOLTAGE_PIN);
-  Serial.println(adc_voltage);
+  //Serial.println(adc_voltage);
 
+  // Transform into Voltage เปลี่ยนเป็นค่าแรงดันไฟฟ้า 
   float real_voltage = ((adc_voltage * 3.3) / (float)(1 << 9));
-  Serial.println(real_voltage);
+  //Serial.println(real_voltage);
 
+  // Linear Calibration Section of Voltage Sensor ส่วนของการปรับเทียบ Voltage Sensor 
   float calibrated_voltage = (real_voltage * a_voltage) + b_voltage;
-  Serial.print("Calibrated Voltage : ");
+  Serial.print("Calibrated Voltage (V) : ");
   Serial.print(calibrated_voltage);
   Serial.println(" V");
   delay(500);
+
+  float Power = calibrated_voltage * calibrated_current;
+  Serial.print("Calibrated Power (W) : ");
+  Serial.print(Power);
+  Serial.println(" W");
+  delay(500);
+}
+
+// Function that for show value from Load Cell Sensor! 
+void ShowLoadCell() {
+  if (scale.is_ready()) {
+    float sum_value = 0;
+    float weight_with_offset = scale.get_units(1);
+    float weight_without_offset = weight_with_offset - 770;
+
+    Serial.print("Weight : ");
+    Serial.print(weight_without_offset);
+    Serial.println(" g.");
+    // delay 500 ms for checking value.
+    delay(500);
+  }
 }
 
 void setup() {
   Serial.begin(9600);
   sensors.begin();
-  scale.begin(dataPin, clockPin);
-  scale.set_scale(2609241);
-  scale.tare(200);
+
   pinMode(CURRENT_PIN, INPUT);
   pinMode(VOLTAGE_PIN, INPUT);
+
+  // initialized Load Cell. เริ่มต้นใช้งาน Load Cell.
+  scale.begin(LOAD_CELL_DATA, LOAD_CELL_CLOCKPIN);
+  scale.set_scale(-118.624);
 }
 
 void loop() {
-  //ShowCurrent();
-  //ShowTemperatures();
-  //ShowVoltage();
-
-
+  // ShowTemperatureFromSensor();
+  // ShowDCPowerMeter();
+  // ShowLoadCell();
 }
--- load_cell_sensor/load_cell_sensor/load_cell_sensor.ino
+++ load_cell_sensor/load_cell_sensor/load_cell_sensor.ino
@@ -1,10 +1,3 @@
-
-//    FILE: HX_is_ready.ino
-//  AUTHOR: Rob Tillaart
-// PURPOSE: HX711 demo
-//     URL: https://github.com/RobTillaart/HX711
-
-
 #include "HX711.h"
 
 HX711 scale;
@@ -12,6 +5,8 @@ HX711 scale;
 //  adjust pins if needed
 uint8_t dataPin = 23;
 uint8_t clockPin = 22;
+float a = 103.51;
+float c = -6191;
 
 
 void setup() {
@@ -25,68 +20,39 @@ void setup() {
   scale.begin(dataPin, clockPin);
 
   //  TODO find a nice solution for this calibration..
-  //  load cell factor 20 KG
-  scale.set_scale(127.15);
+  //load cell factor 20 KG
+  scale.set_scale(-118.624);
 
   //  load cell factor 5 KG
-  /
-//    FILE: HX_is_ready.ino
-//  AUTHOR: Rob Tillaart
-// PURPOSE: HX711 demo
-//     URL: https://github.com/RobTillaart/HX711
-
-
-#include "HX711.h"
-
-HX711 scale;
-
-//  adjust pins if needed
-uint8_t dataPin = 23;
-uint8_t clockPin = 22;
-
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println(__FILE__);
-  Serial.print("HX711_LIB_VERSION: ");
-  Serial.println(HX711_LIB_VERSION);
-  Serial.println();
-
-  scale.begin(dataPin, clockPin);
-
-  //  TODO find a nice solution for this calibration..
-  //  load cell factor 20 KG
-  //  scale.set_scale(127.15);
-
-  //  load cell factor 5 KG
-  scale.set_scale(107.605881);  //  TODO you need to calibrate this yourself.
-  scale.set_offset(-85735);
+  // scale.set_scale(107.605881);  //  TODO you need to calibrate this yourself.
+  // scale.set_offset(-85735);
   //  reset the scale to zero = 0
   // scale.tare(20);
 }
 
 
 void loop() {
+
+  // create condition for checking my load cell is ready ? show value below : my load cell'll broken down.
   if (scale.is_ready()) {
-    Serial.println(scale.get_units(1)*-1);
-    delay(2000);
+    float sum_value = 0;
+    float weight_with_offset = scale.get_units(1);
+    float weight_without_offset = weight_with_offset - 770;
+
+    float weight_with_calibrated = (a*weight_with_offset)+c;
+
+
+    Serial.print("Mean Weight : ");
+    Serial.print(weight_without_offset);
+    Serial.println(" g.");
+    // delay 500 ms for checking value.
+    delay(500);
   }
 }
 
 
-//  -- END OF FILE --
-  //  reset the scale to zero = 0
-  // scale.tare(20);
-}
-
-
-void loop() {
-  if (scale.is_ready()) {
-    Serial.println(scale.get_units(1)*-1);
-    delay(2000);
-  }
-}
 
 
 //  -- END OF FILE --
+//  reset the scale to zero = 0
+// scale.tare(20);
